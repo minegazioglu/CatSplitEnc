13715acd700a14e3c214752cad7b43a37178cc38
minegazioglu <56631715+minegazioglu@users.noreply.github.com>
2022-06-04 13:14:30 +0300

    Update kdd_internet_usage.r

--- datasets/kdd_internet_usage.r
+++ datasets/kdd_internet_usage.r
@@ -64,6 +64,6 @@ uploadOMLDataSet(new_oml_dat, verbosity = 2)
 
 # Uploading data set to server.
 # Uploading to 'http://www.openml.org/api/v1/data'.
-# Data set successfully uploaded. Data set ID: 43909
+# Data set successfully uploaded. Data set ID: 43920
 
-#deleteOMLObject(43909, object = c("data"), verbosity = NULL)
+#deleteOMLObject(43920, object = c("data"), verbosity = NULL)
